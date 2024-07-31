d943ce28fc76b62a6d69e44eb87da2d713340b29
aniketbhurle <bhurle.aniket@gmail.com>
2024-08-01 02:46:21 +0530

    Adding ArrayList/HashTable Practice

---
+++ Array_List_Practice/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("\nArrayList Practice");
+
+        ArrayList arrayList = new ArrayList();
+
+        arrayList.Add(101);
+        arrayList.Add("Aniket");
+        arrayList.Add(" ");
+        arrayList.Add(4.6);
+
+        arrayList.Add(null);
+
+        Console.WriteLine("\n\n++We can add any values to arraylist the therefore its non-generic: \nAdding elements using .Add()\nArrayList1: ");
+        foreach(var i in arrayList)
+        {
+            Console.Write(i+" ");
+        }
+
+        Console.WriteLine("\n\nAdding Elements thriugh object initializer: \nArrayList2: ");
+        var ArrayList2 = new ArrayList { 102, "Simth", "Jpne", true, 15.22, 456 };
+        foreach(var j in ArrayList2)
+        {
+            Console.Write(j+" ");
+        }
+
+
+        Console.WriteLine("\n\nAdding Elements in bwetween: \nArrayList3: ");
+        var ArrayList3 = new ArrayList { 102, "Simth", "Jpne", true, 15.22, 456 };
+
+        ArrayList3.Insert(2,"Third POsition"); 
+
+        foreach (var j in ArrayList3)
+        {
+            Console.Write(j + " ");
+        }
+
+
+
+        Console.WriteLine("\n\nAdding Range of Elements from 2nd position in bwetween: \nArrayList3: ");
+        var ArrayList4 = new ArrayList { 102, "Simth", "Jpne", true, 15.22, 456 };
+
+        ArrayList4.InsertRange(2, arrayList );
+
+        foreach (var j in ArrayList4)
+        {
+            Console.Write(j + " ");
+        }
+
+
+        Console.ReadKey();
+    }
+}
---
+++ Hashtable_Practice/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("HashTable Practice\n\n");
+
+
+        ///Hashtable Example is as follows:
+        ///
+
+        Hashtable Ht = new Hashtable();
+
+        Ht.Add("EmpID",1000);
+        Ht.Add("EmpName","Jone");
+        Ht.Add("EmpSalary",10000);
+        Ht.Add("Location","Norway");
+
+
+        Console.WriteLine("\nHashtable is published in Key Value Pair for traversing we need Key\nHashtable is like a dic\n\nHashtable1: ");
+        foreach(var i in Ht.Keys)
+        {
+            Console.WriteLine("Key: "+i+" Value:" + Ht[i]);
+        }
+
+        ///Printing the independent Values based on Keys
+        Console.WriteLine("\n\n\nEmployee Location based on the Key: ");
+        Console.WriteLine("The location of the Employee is as follows: {0}\n\n", Ht["Location"]);
+
+        ///
+        ///Adding the elemets to the Hashtable
+        Ht.Add("Office Locations","Pune, Mumbai, Hyd, Bangalore");
+        Console.WriteLine("Added new list of elements to the Ht above Key naming: Office Locations - lets check ");
+        Console.WriteLine("The office Locations is as follows: {0}\n\n", Ht["Office Locations"]);
+
+        ////We can remove the objects with the help of key => HashtableName.Remove(Key)
+        ///
+
+        ////We can also assign values to the hashtable like Ht[30] = "Value";
+        Console.WriteLine("values to the hashtable like Ht[30] = Value\n\n\n");
+        Ht[30] = "Index Value";
+        foreach (var i in Ht.Keys)
+        {
+            Console.WriteLine("Key: " + i + "\t Value:" + Ht[i]);
+        }
+
+        ////IMP for DictonaryEntry the value of KEYS should be INT !!***
+
+        Console.ReadKey();
+    }
+}
