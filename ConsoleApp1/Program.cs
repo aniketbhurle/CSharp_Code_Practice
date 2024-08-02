942e6dc17d56bebdfd91c242847567df4cef13a8
aniketbhurle <bhurle.aniket@gmail.com>
2024-08-02 23:21:12 +0530

    Added the Dictonary task performed

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,18 +6,19 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			//Practicing Array
-			int[] array1 = { 1,2,3,4, 5};
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Practicing Array
+            int[] array1 = { 1, 2, 3, 4, 5 };
 
-			foreach (int i in array1)
-			{
-				Console.WriteLine("{0} ",i);
-			}
+            foreach (int i in array1)
+            {
+                Console.WriteLine("{0} ", i);
+            }
 
-		}
-	}
+            Console.ReadKey();
+        }
+    }
 }
---
+++ Generic_Dictonary/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Linq;
+
+namespace Generic_Dictonary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dictionary Demos: ");
+
+
+            Dictionary<string, string> dict_1 = new Dictionary<string, string>()
+            {
+                {"India","Delhi, Pune Maharashtra, MP, Rajasthan"},
+                {"USA","Chicago, New York, Washington " },
+                {"UK", "London Manchester" }
+            };
+
+            Console.WriteLine("\nDictonary Acccessing the by foreach loop: ");
+            foreach (KeyValuePair<string, string> kvp in dict_1)
+            {
+                Console.WriteLine("Key: "+kvp.Key+" Value: "+kvp.Value);
+            }
+
+            dict_1.Add("New Country", "New Cities"); //Adding new value
+            dict_1["NULL"] = "Null Values to be added";
+
+            if(dict_1.ContainsKey("USA"))
+            {
+                Console.WriteLine("\n++The USA key is there in the dict");
+            }
+            else
+            {
+                Console.WriteLine("\nNot available in dict");
+            }
+
+            Console.WriteLine("\n++Accessing the dictonary with AsParallel().ForAll():\n");
+            dict_1.AsParallel().ForAll(dict => Console.WriteLine(dict.Key+" : "+dict.Value));
+
+            Dictionary<int, Employee> emp_1 = new Dictionary<int, Employee>()
+            {
+                {101, new Employee{empID=1000, empGender="Male", empName="Aniket" } },
+                {106, new Employee{empID=1023, empGender="Male", empName="Amol" } },
+                {104, new Employee{empID=10245, empGender="Female", empName="AJanema" } },
+                {100, new Employee{empID=1020, empGender="Male", empName="Jon" } }
+            };
+
+
+            Console.WriteLine("\n++Accessing the dictonary as Complex:\n");
+            foreach (KeyValuePair<int,Employee> kvp in emp_1)
+            {
+                Console.WriteLine(kvp.Key+" : "+kvp.Value.empID + " : " +kvp.Value.empName + " : " +kvp.Value.empGender);
+            }
+
+
+            Console.WriteLine("\nSorting the Dictonary with LINQ :"); 
+
+            var emp_11 = emp_1.ToList();
+            emp_11 = emp_11.OrderBy(x => x.Value.empID).ToList();
+            foreach (var item in emp_11)
+            {
+                Console.WriteLine(item.Key+" : "+item.Value.empName + " : " +item.Value.empID + " " +item.Value.empGender);
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+
+        public class Employee
+        {
+            public int empID { get; set; }
+            public string empName { get; set; }
+            public string empGender { get; set; }
+        }
+    }
+}
