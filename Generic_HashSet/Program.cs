8cbab5dd0d6ef168c87872d11c650f9d94bb5cbe
aniketbhurle <bhurle.aniket@gmail.com>
2024-08-03 03:36:14 +0530

    added HashSet

---
+++ Generic_HashSet/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Generic_HashSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Generic HashSet");
+
+
+            HashSet<string> hset = new HashSet<string>()
+            {"James", "Thor","Lars", "Oyvidn","Lars" };
+
+
+            Console.WriteLine("\nAccessing the HashSet using foreach: ");
+            foreach (string h in hset)
+            {
+                Console.Write(h+" ");
+            }
+
+            hset.Add("Lars"); //will not throw any error but will not add only UNIQUE values
+
+
+            int hset_1 = hset.RemoveWhere(x => x.StartsWith("L"));
+
+            Console.WriteLine("\n\nAfter removing the element starting from 'L' : ");
+            
+            foreach (string h in hset)
+            {
+                Console.Write(h + " ");
+            }
+
+
+
+            Console.WriteLine("\n\nPrinting with only if condition i.e. Contains condition: ");
+            if (hset.Contains("Thor"))
+            {
+                Console.WriteLine("\nYes the keyword is there in the HashSet");
+            }
+            else
+            {
+                Console.WriteLine("\nDoesn't have the keyword");
+            }
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
